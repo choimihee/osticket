59ccb9be5ebeffeea7f03248a87c3456ad63a862
choimihee <71679647+choimihee@users.noreply.github.com>
2020-10-13 16:10:33 +0900

    Update class.ostsession.php

--- include/class.ostsession.php
+++ include/class.ostsession.php
@@ -205,8 +205,11 @@ extends SessionBackend {
         // Create a session data obj if not loaded.
         if (!isset($this->data))
             $this->data = new SessionData(['session_id' => $id]);
-
-        $this->data->session_data = $data;
+# 로그인오류로 인하여 수정 2020.10.13        
+#        $this->data->session_data = $data;
+        catch (DoesNotExist $e) {
+         $this->data = new SessionData(['session_id' => $id]);
+          $this->data->session_data = "";
         $this->data->session_expire =
             SqlFunction::NOW()->plus(SqlInterval::SECOND($ttl));
         $this->data->user_id = $thisstaff ? $thisstaff->getId() : 0;
